f28788361c2d3dc145b64503722d50c4c24cd11e
vanelus <njikekamgnia@gmail.com>
2014-11-01 00:28:20 +0100

    Basic layout to read a kaassa hotel from a json file.

--- KaassaMobile/src/com/kaassa/mobile/KaassaMobile.java
+++ KaassaMobile/src/com/kaassa/mobile/KaassaMobile.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.res.Resources;
 import android.net.ConnectivityManager;
@@ -11,8 +15,8 @@ import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.kaassa.mobile.lib.gson-2.3.jar;
 
 public class KaassaMobile extends Activity
 {
@@ -46,25 +50,126 @@ public class KaassaMobile extends Activity
           
         String result = str.toString();
         
-        Log.i("JsonTest: ",result);
+    //    Log.i("JsonTest: ",result);
         
-        Resources res = getResources();
-        TextView vue = (TextView)findViewById(R.id.jsonDisplay);
-        vue.setText(result);
+
         
         // parser le json
+        JSONObject reader = null;
+		try {
+			reader = new JSONObject(result);
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
         
-        InputStream inputStream = str ;
-		if(inputStream != null) {
-            // Lecture de l'inputStream dans un reader
-            InputStreamReader reader = new InputStreamReader(inputStream);
-
-            // Return la liste désérialisé par le moteur gson
-             gson.fromJson(reader, new TypeToken<List<Point>>(){}.getType());
+        String Hotel_Name = null;
+		try {
+			Hotel_Name = reader.getString("name");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		String Hotel_Star = null;
+        try {
+			 Hotel_Star = reader.getString("stars");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+        //Get the hotel location
+        JSONObject Hotel_Location = null;
+        try {
+			 Hotel_Location  = reader.getJSONObject("location");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //Get the hotel contacts
+        JSONObject Hotel_contacts = null;
+        try {
+			 Hotel_contacts  = reader.getJSONObject("contact");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //Get the hotel services
+        JSONArray Hotel_services = null;
+        try {
+			 Hotel_services = reader.getJSONArray("services");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //Get the hotel billing
+        JSONObject Hotel_billing = null;
+        try {
+			 Hotel_billing  = reader.getJSONObject("billing");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        
+        
+        
+      //display in long period of time
+        Toast.makeText(getApplicationContext(),"Hotel :"+Hotel_Name+" with "+Hotel_Star+"stars", Toast.LENGTH_LONG).show();
+               
+        
+        TextView HotelNameValue = (TextView)findViewById(R.id.HotelNameValue);
+        TextView HotelLocationValue = (TextView)findViewById(R.id.HotelLocationValue);
+        TextView HotelContactValue = (TextView)findViewById(R.id.HotelContactValue);
+        TextView HotelServicesValue = (TextView)findViewById(R.id.HotelServicesValue);
+        TextView HotelBillingValue = (TextView)findViewById(R.id.HotelBillingValue);
+        TextView HotelStarsValue = (TextView)findViewById(R.id.HotelStarsValue);
+        
+        HotelNameValue.setText(Hotel_Name);
+        try {
+			HotelLocationValue.setText(Hotel_Location.getString("address"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			HotelContactValue.setText(Hotel_contacts.getString("phone_one"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        //construire la chaine de services d'un hotel
+        String TbServices = "";
+        for(int i=0;i<Hotel_services.length();i++){
+			try {
+				TbServices = TbServices + Hotel_services.getJSONObject(i).getString("name_f_r");
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
         }
+			HotelServicesValue.setText(TbServices);
+	
+        try {
+			HotelBillingValue.setText(Hotel_billing.getString("price_min"));
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        HotelStarsValue.setText(Hotel_Star);
+        
+        
+//        setContentView(HotelNameValue);
+//        setContentView(HotelStarsValue);
+        
 
-    } catch (Exception e) {
-        Log.e("WebService", "Impossible de rapatrier les données");
+        
+        
              
     }
     
@@ -78,13 +183,7 @@ public class KaassaMobile extends Activity
 
     	
     }
-    
-    public class Cake  
-    {  
-        public int id;  
-        public String name;  
-    }  
-    
+   
     // check network connection
     public boolean isConnected(){
         ConnectivityManager connMgr = (ConnectivityManager) getSystemService(this.CONNECTIVITY_SERVICE);
