1e15311956dab4e57b36a86e1dfc77fd142693d1
Vanel NJIKE <njikekamgnia@gmail.com>
2014-11-11 07:29:18 +0100

    -creation d'un objet hotel (class hotelRecord) - creation d'une vue d'affichage d'une ligne d'hotel. (res > hotellist.xm) - creation d'un adapteur entre notre vue et une ligne d'hotel à afficher (class HotelAdapter)

--- KaassaMobile/src/com/kaassa/mobile/KaassaMobile.java
+++ KaassaMobile/src/com/kaassa/mobile/KaassaMobile.java
@@ -3,6 +3,8 @@ package com.kaassa.mobile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,12 +16,15 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class KaassaMobile extends Activity
 {
+	HotelAdapter HotelAdapter;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -27,151 +32,130 @@ public class KaassaMobile extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
-        //get the json text -in asset rep
-        InputStream is = null;
-		try {
-			is = this.getAssets().open("apex-hilton.json");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
+    	ListView listView = (ListView)findViewById(R.id.hotels_list);
+    	HotelAdapter = new HotelAdapter(getBaseContext());
+    	listView.setAdapter(HotelAdapter);
+    	
+    
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listHotel);
+//        liste.setAdapter(adapter);
         
-        // check the value
-        int ch;  
-        StringBuffer str = new StringBuffer();  
-          
-        try {
-			while ((ch = is.read()) != -1)  
-			    str.append((char) ch);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-          
-        String result = str.toString();
-        
-    //    Log.i("JsonTest: ",result);
-        
-
-        
-        // parser le json
-        JSONObject reader = null;
-		try {
-			reader = new JSONObject(result);
-		} catch (JSONException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-        
-        String Hotel_Name = null;
-		try {
-			Hotel_Name = reader.getString("name");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String Hotel_Star = null;
-        try {
-			 Hotel_Star = reader.getString("stars");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        
-        //Get the hotel location
-        JSONObject Hotel_Location = null;
-        try {
-			 Hotel_Location  = reader.getJSONObject("location");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        //Get the hotel contacts
-        JSONObject Hotel_contacts = null;
-        try {
-			 Hotel_contacts  = reader.getJSONObject("contact");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        //Get the hotel services
-        JSONArray Hotel_services = null;
-        try {
-			 Hotel_services = reader.getJSONArray("services");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        //Get the hotel billing
-        JSONObject Hotel_billing = null;
-        try {
-			 Hotel_billing  = reader.getJSONObject("billing");
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        
-        
-        
-      //display in long period of time
-        Toast.makeText(getApplicationContext(),"Hotel :"+Hotel_Name+" with "+Hotel_Star+"stars", Toast.LENGTH_LONG).show();
-               
-        
-        TextView HotelNameValue = (TextView)findViewById(R.id.HotelNameValue);
-        TextView HotelLocationValue = (TextView)findViewById(R.id.HotelLocationValue);
-        TextView HotelContactValue = (TextView)findViewById(R.id.HotelContactValue);
-        TextView HotelServicesValue = (TextView)findViewById(R.id.HotelServicesValue);
-        TextView HotelBillingValue = (TextView)findViewById(R.id.HotelBillingValue);
-        TextView HotelStarsValue = (TextView)findViewById(R.id.HotelStarsValue);
-        
-        HotelNameValue.setText(Hotel_Name);
-        try {
-			HotelLocationValue.setText(Hotel_Location.getString("address"));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        try {
-			HotelContactValue.setText(Hotel_contacts.getString("phone_one"));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-        //construire la chaine de services d'un hotel
-        String TbServices = "";
-        for(int i=0;i<Hotel_services.length();i++){
-			try {
-				TbServices = TbServices + Hotel_services.getJSONObject(i).getString("name_f_r");
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-			HotelServicesValue.setText(TbServices);
-	
-        try {
-			HotelBillingValue.setText(Hotel_billing.getString("price_min"));
-		} catch (JSONException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        HotelStarsValue.setText(Hotel_Star);
-        
-        
+//        JSONObject reader = null;
+//		try {
+//			reader = new JSONObject(result);
+//		} catch (JSONException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+//        
+//        String Hotel_Name = null;
+//		try {
+//			Hotel_Name = reader.getString("name");
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		String Hotel_Star = null;
+//        try {
+//			 Hotel_Star = reader.getString("stars");
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        
+//        //Get the hotel location
+//        JSONObject Hotel_Location = null;
+//        try {
+//			 Hotel_Location  = reader.getJSONObject("location");
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        //Get the hotel contacts
+//        JSONObject Hotel_contacts = null;
+//        try {
+//			 Hotel_contacts  = reader.getJSONObject("contact");
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        //Get the hotel services
+//        JSONArray Hotel_services = null;
+//        try {
+//			 Hotel_services = reader.getJSONArray("services");
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        //Get the hotel billing
+//        JSONObject Hotel_billing = null;
+//        try {
+//			 Hotel_billing  = reader.getJSONObject("billing");
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        
+//        
+//        
+//      //display in long period of time
+//        Toast.makeText(getApplicationContext(),"Hotel :"+Hotel_Name+" with "+Hotel_Star+"stars", Toast.LENGTH_LONG).show();
+//               
+//        
+//        TextView HotelNameValue = (TextView)findViewById(R.id.HotelNameValue);
+//        TextView HotelLocationValue = (TextView)findViewById(R.id.HotelLocationValue);
+//        TextView HotelContactValue = (TextView)findViewById(R.id.HotelContactValue);
+//        TextView HotelServicesValue = (TextView)findViewById(R.id.HotelServicesValue);
+//        TextView HotelBillingValue = (TextView)findViewById(R.id.HotelBillingValue);
+//        TextView HotelStarsValue = (TextView)findViewById(R.id.HotelStarsValue);
+//        
+//        HotelNameValue.setText(Hotel_Name);
+//        try {
+//			HotelLocationValue.setText(Hotel_Location.getString("address"));
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        try {
+//			HotelContactValue.setText(Hotel_contacts.getString("phone_one"));
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        
+//        //construire la chaine de services d'un hotel
+//        String TbServices = "";
+//        for(int i=0;i<Hotel_services.length();i++){
+//			try {
+//				TbServices = TbServices + Hotel_services.getJSONObject(i).getString("name_f_r");
+//			} catch (JSONException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//        }
+//			HotelServicesValue.setText(TbServices);
+//	
+//        try {
+//			HotelBillingValue.setText(Hotel_billing.getString("price_min"));
+//		} catch (JSONException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//        HotelStarsValue.setText(Hotel_Star);
+//        
+//        
 //        setContentView(HotelNameValue);
 //        setContentView(HotelStarsValue);
-        
+//        
+    }
 
         
         
-             
-    }
     
     public void onStart(Bundle savedInstanceState)
     {
